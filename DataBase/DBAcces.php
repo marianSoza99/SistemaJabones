9bf0fb035288497e95bc8d237c0ef6ac7219d5f9
agonizedB22 <47332059+agonizedB22@users.noreply.github.com>
2020-07-23 13:43:02 -0600

    Conexión bases de datos

--- DataBase/DBAcces.php
+++ DataBase/DBAcces.php
@@ -5,9 +5,21 @@
         private static function connect(){
             $config=parse_ini_file('config.ini');
             $localhost=$config['host'];
-            $conn = new PDO("mysql:host=$localhost;dbname=myDB", $config['username'], $config['password']);
+            $database=$config['database'];
+            $conn = new PDO("mysql:host=$localhost;dbname=$database", $config['username'], $config['password']);
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             return $conn;
+        }
+
+        public static function agregarUsuario(){
+            try{
+                $conn=self::connect();
+                $sql="";
+            }catch(PDOException $e){
+                echo $sql . "<br>" . $e->getMessage();
+            }finally{
+                $conn=null;
+            }
         }
 
         
